21f5052604f7767a172c20487b32588e7406ef2d
gungunzp <flavazp@gmail.com>
2019-12-26 12:35:05 +0200

    course exercises only

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 # Fullstack GraphQL
+
 > Learn how to use GraphQL with Node and React
 
 This course comes with some [slides](https://docs.google.com/presentation/d/1IrGA4PtUEZPVDTBg5_WCMmUapElbFBgLwfSBAp8ft1g/edit?usp=sharing)
 
-
 ## What you'll need
-* Node version >= 6
+
+- Node version >= 6
 
 ## Solutions
+
 The solution branch has the completed course fo reference. There is no one way to finish this course.
 `git checkout solution`
+
+### Origin:
+
+from course: https://coursehunter.net/course/client-side-graphql-v-react
+
+repo: https://github.com/FrontendMasters/fullstack-graphql
--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -1,21 +1,95 @@
-import React, {useState} from 'react'
-import gql from 'graphql-tag'
-import { useQuery, useMutation } from '@apollo/react-hooks'
-import PetsList from '../components/PetsList'
-import NewPetModal from '../components/NewPetModal'
-import Loader from '../components/Loader'
+import React, { useState } from 'react';
+import gql from 'graphql-tag';
+import { useQuery, useMutation } from '@apollo/react-hooks';
+import PetsList from '../components/PetsList';
+import NewPetModal from '../components/NewPetModal';
+import Loader from '../components/Loader';
 
+const PETS_FIELDS = gql`
+  fragment PetsFields on Pet {
+    id
+    name
+    type
+    img
+    vaccinated @client
+    owner {
+      id
+      age @client
+    }
+  }
+`;
 
-export default function Pets () {
-  const [modal, setModal] = useState(false)
+const ALL_PETS = gql`
+  query AllPets {
+    pets {
+      ...PetsFields
+    }
+  }
+  ${PETS_FIELDS}
+`;
 
+const NEW_PET = gql`
+  mutation CreateAPet($newPet: NewPetInput!) {
+    addPet(input: $newPet) {
+      ...PetsFields
+    }
+  }
+  ${PETS_FIELDS}
+`;
+
+export default function Pets() {
+  const [modal, setModal] = useState(false);
+  const { data, loading, error } = useQuery(ALL_PETS);
+  const [createPet, newPet] = useMutation(NEW_PET, {
+    update(cache, { data: { addPet } }) {
+      const { pets } = cache.readQuery({ query: ALL_PETS });
+      cache.writeQuery({
+        query: ALL_PETS,
+        data: { pets: [addPet, ...pets] }
+      });
+    },
+    optimisticResponse: {
+      __typename: 'Mutation',
+      addPet: {
+        id: Math.floor(Math.random() * 10000) + '',
+        name: 'from mutation',
+        type: 'CAT',
+        img: 'https://via.placeholder.com/300',
+        __typename: 'Pet'
+      }
+    }
+  });
 
   const onSubmit = input => {
-    setModal(false)
+    setModal(false);
+    createPet({
+      variables: { newPet: input }
+      // optimisticResponse: {
+      //   __typename: 'Mutation',
+      //   addPet: {
+      //     id: Math.floor(Math.random() * 10000) + '',
+      //     name: input.name,
+      //     type: input.type,
+      //     img: 'https://via.placeholder.com/300',
+      //     // img: '',
+      //     __typename: 'Pet'
+      //   }
+      // }
+    });
+  };
+
+  if (loading /*  || newPet.loading*/) {
+    return <Loader />;
   }
-  
+
+  if (error || newPet.error) {
+    return <p>error!</p>;
+  }
+
+  console.log(data.pets[0]);
+
   if (modal) {
-    return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />
+    return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />;
   }
 
   return (
@@ -32,8 +106,8 @@ export default function Pets () {
         </div>
       </section>
       <section>
-        <PetsList />
+        <PetsList pets={data.pets} />
       </section>
     </div>
-  )
+  );
 }
